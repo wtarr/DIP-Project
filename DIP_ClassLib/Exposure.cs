3971a980e963554afdd7cf38ea55ea353484ed63
wtarr <william.projects.2012@gmail.com>
2013-09-24 23:04:05 +0100

    Add a class to encapsulate exposure (not yet implemented methods)

---
+++ DIP_ClassLib/Exposure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace DIP_ClassLib
+{
+    public class Exposure
+    {
+
+        // Brighten
+        public Bitmap Brighten(Bitmap orig)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Darken
+        public Bitmap Darken(Bitmap orig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
